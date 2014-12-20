592652dd2fe3cd7004145fec4451a36114d18ffe
DouglasRMiles <miles.douglas.r@gmail.com>
2014-12-19 23:14:14 -0800

    KnowRob MUD

--- knowrob_mud/prolog/init.pl
+++ knowrob_mud/prolog/init.pl
@@ -1,201 +1,44 @@
+%%
+%% Copyright (C) 2013 by Moritz Tenorth
+%%
+%% This program is free software; you can redistribute it and/or modify
+%% it under the terms of the GNU General Public License as published by
+%% the Free Software Foundation; either version 3 of the License, or
+%% (at your option) any later version.
+%%
+%% This program is distributed in the hope that it will be useful,
+%% but WITHOUT ANY WARRANTY; without even the implied warranty of
+%% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+%% GNU General Public License for more details.
+%%
+%% You should have received a copy of the GNU General Public License
+%% along with this program.  If not, see <http://www.gnu.org/licenses/>.
+%%
 
-/** <module> knowrob_mud - An Implementation of a MUD server
+/** <module> knowrob_mud/init.pl - An Implementation of a MUD server
 
 
 @license LGPL
 */
 
-:- set_prolog_flag(file_loading,verbose).
-:- ensure_loaded('../../../../../src_lib/logicmoo_util/logicmoo_util_all.pl').
-:- abolish(dbase_rosprolog,1).
-:- dynamic(dbase_rosprolog/1). 
-:- prolog_load_context(file, Here),asserta(dbase_rosprolog(Here)).
-dbase_rosprolog_dir(DIR):- dbase_rosprolog(Here),absolute_file_name(Here,AHere),file_directory_name(AHere,DIR).
+% This is for when PrologMUD starts KnowRob (instead of the other way around)
+:- current_predicate(register_ros_package/1) -> true; consult(rosprolog).
 
-user:file_search_path(knowrob,Where):- dbase_rosprolog(Here),file_directory_name(Here,DIR),concat_paths(DIR,'../../../*/',OOO),enumerate_files(OOO,Where).
-user:file_search_path(pack, knowrob).
-user:file_search_path(ros, knowrob).
-
-% :-asserta((library_directory(Where):-ros_library_directory(Where))).
-user:library_directory(Where):-ros_library_directory(Where).
-
-:- dynamic(ros_package_initialized/1).
-
-% enumerate_files(Spec,Result):- \+ exists_file_or_dir(Spec),!,expand_file_search_path(Spec,Result).
-enumerate_files(Spec,Result):-expand_file_name(Spec,ResultList),member(NResult,ResultList),normalize_path(NResult,Result).
-
-absolute_file_name_or_dir_opts(Spec,File,Options):-absolute_file_name(Spec,File,[file_type(directory),file_errors(fail)|Options])*->true;absolute_file_name(Spec,File,Options).
-
-absolute_file_name_or_dir(Spec,File):-absolute_file_name(Spec,File,[file_type(directory),file_errors(fail)])*->true;absolute_file_name(Spec,File).
-
-exists_file_or_dir(X):-exists_file(X),!.
-exists_file_or_dir(X):-is_directory(X).
-is_directory(X):-exists_directory(X).
-
-concat_paths(ParentIn,Child,Result):- enumerate_files(ParentIn,Parent),
-   (is_directory(Parent) -> directory_file_path(Parent,Child,Joined) ; atom_concat(Parent,Child,Joined)),
-   enumerate_files(Joined,Result).
-
-concat_paths([Joined],Result):- !,enumerate_files(Joined,Result).
-concat_paths([ParentIn,Child|MORE],Result):- concat_paths(ParentIn,Child,ResultM),concat_paths([ResultM|MORE],Result).
-
-
-ros_library_directory(WhereF2):-user:file_search_path(knowrob,W),once((atom_concat(W,'/*/prolog/',O),
-  expand_file_name(O,ListE))),member(Where,ListE),normalize_path(Where,WhereF2).
-
-normalize_path(Where,WhereF2):- absolute_file_name(Where,WhereF),prolog_to_os_filename(WhereF,WhereF1),prolog_to_os_filename(WhereF2,WhereF1),!.
-normalize_path(Where,Where):-!.
-
-ros_package(B):-ros_library_directory(WhereF2),concat_paths(WhereF2,(..),O),file_base_name(O,B).
-
-ros_package_initfile(PackagePath,InitFile):-
-  concat_paths([PackagePath,'./prolog/init{.pl,.pl.in,}'],Where),expand_file_name(Where,[InitFile|_]),!.
-ros_package_initfile(RosPack,InitFile):-
-  expand_file_search_path(knowrob(RosPack/prolog/'init{.pl,.pl.in,}'),Where),expand_file_name(Where,[InitFile|_]).
-
-ros_package_dir(RosPack,Dir):- ros_package_initfile(RosPack,InitFile),concat_paths([InitFile,'./../..'],Dir).
-
-
-
-%% rospack_package_path(+Package, -Path) is nondet.
-%
-% Locate ROS packages on the harddisk using 'rospack find'
-% 
-% @param Package  Name of a ROS package
-% @param Path     Global path of Package determined by 'rospack find'
-% 
-rospack_package_path(Package, Path) :- ros_package_dir(Package, Path),must(exists_directory(Path)).
-rospack_package_path(Package, Path) :- expand_file_search_path(knowrob(Package),Path),exists_directory(Path).
-rospack_package_path(Package, Path) :- must(rospack_find_package_path(Package, Path)).
-rospack_find_package_path(Package, Path) :-
-  nonvar(Package),
-  process_create(path('rospack'), ['find', Package], [stdout(pipe(RospackOutput)), process(PID)]),
-  read_line_to_codes(RospackOutput, C),
-  string_to_list(Path, C),
-  process_wait(PID, _).
-
-%% init_ros_package(+PackagePath) is nondet.
-%
-% Initialize a KnowRob package by consulting the prolog/init.pl file
-% 
-% @param PackagePath  Path towards the package to be initialized
-% 
-init_ros_package( Package ) :- do_not_register_ros_package( Package )  ,!.
-init_ros_package( PackagePath ) :-
-  (ros_package_initfile(PackagePath,InitFile);atom_concat(PackagePath, 'init.pl', InitFile)),
-  exists_file(InitFile),
-  must(consult(InitFile)), !.
-
-init_ros_package( _ ).
-
-%% register_ros_package(+Package) is nondet.
-%% register_ros_package(+Package, ?AbsoluteDirectory) is nondet.
-%
-% Find and initialize a KnowRob package, i.e. locate the package on the
-% harddisk, add the path to the library search path, and consult the init.pl
-% file that (recursively) initializes the package and its dependencies.
-% 
-% @param Package            Name of a ROS package
-% @param AbsoluteDirectory  Global path to the 'prolog' subdirectory in the given package
-% 
-
-do_not_register_ros_package( rosprolog ) :-!.
-% do_not_register_ros_package( semweb ) :-!.
-% do_not_register_ros_package( jpl ) :-!.
-% do_not_register_ros_package( thea ) :-!.
-
-register_ros_package( Package, _ ) :- do_not_register_ros_package( Package )  ,!.
-
-register_ros_package(Package, _) :-
-  current_predicate(ros_package_initialized/1),
-  ros_package_initialized(Package), !.
-
-register_ros_package(Package, AbsoluteDirectory) :-
-  rospack_package_path(Package, PackagePath),
-  nonvar(PackagePath),
-  atom_concat(PackagePath, '/prolog/', AbsoluteDirectory),
-  asserta(library_directory(AbsoluteDirectory)),
-  assert(user:file_search_path(ros, AbsoluteDirectory)),
-  assert( ros_package_initialized(Package) ),
-  add_ros_package_to_classpath(Package),
-  init_ros_package( AbsoluteDirectory ).
-
-
-register_ros_package(Package) :-
-  register_ros_package(Package, _).
-
-use_ros_module(Package, FilePath) :-
-  register_ros_package(Package, AbsoluteDirectory),
-  atom_concat(AbsoluteDirectory, FilePath, AbsoluteFilePath),
-  use_module( AbsoluteFilePath ).
-
-
-
-%% add_ros_package_to_classpath(+Package) is nondet.
-% 
-% Adds Java dependencies of Package to the CLASSPATH environment variable
-%
-% @param Package Name of a ROS package
-% 
-add_ros_package_to_classpath(Package):-
-	rospack_package_classpath(Package, Path),
-	atom_concat(':',Path,PackagePath),
-	setenv("CLASSPATH",PackagePath).
-
-%% rospack_package_classpath(+Package, -Path) is nondet.
-% 
-% Calculates the Java dependencies of Package and returns a string to be appended to the CLASSPATH
-%
-% @param Package  Name of a ROS package
-% @param Path     String with the dependencies to be added to the CLASSPATH
-% 
-% calculates java dependencies for classpath
-rospack_package_classpath(Package, Path) :- ros_package_dir(Package, PackPath),atom_concat(PackPath,'**/*.jar',O),!,
-  expand_file_name(O,ListE),atomic_list_concat(ListE,':',Path).
-rospack_package_classpath(Package, Path) :-
-  nonvar(Package),
-  process_create(path('rosrun'), ['rosprolog', 'get_pkg_classpath', Package], [stdout(pipe(RospackOutput)), process(PID)]),
-  read_line_to_codes(RospackOutput, C),
-  string_to_list(Path, C),
-  process_wait(PID, _).
-
-
-% concat a value to an environment varible
-% please note: delimiters have to be set within Val, e.g.:
-% ':/path/to/lib:/path/to/lib2'
-concat_env(Var,Val):-
-	(getenv(Var,OldVal)
-	->  (atom_concat(OldVal,Val,NewVal)) ;
-        (NewVal = Val)),
-	setenv(Var,NewVal).
-
-
-:- register_ros_package(knowrob_roslog_launch).
-:- register_ros_package(knowrob_cram).
-:- register_ros_package(knowrob_sim).
+:- register_ros_package(knowrob_common).
 :- register_ros_package(knowrob_mongo).
-:- register_ros_package(knowrob_vis).
-:- register_ros_package(iai_semantic_maps).
-:- register_ros_package(knowrob_srdl).
-:- show_call(forall(user:file_search_path(knowrob,_Where),true)).
+:- register_ros_package(comp_temporal).
+:- register_ros_package(knowrob_sim).
+
+:- register_ros_package(knowrob_mud).
 
 
-end_of_file.
+:- use_module(library('knowrob_mud')).
 
-:- init_ros_package(knowrob_common).
-% :- init_ros_package(prolog_perception).
-% :- init_ros_package(knowrob_actions).
-% :- init_ros_package(ias_knowledge_base).
-% :- init_ros_package(comp_missingobj).
-% :- init_ros_package(comp_semantic_map).
-% :- init_ros_package(semweb).
-:- forall(ros_package(B),init_ros_package(B)).
+%Extended ontology
+:- owl_parser:owl_parse('package://knowrob_mud/owl/knowrob_mud.owl').
+:- rdf_db:rdf_register_ns(mud, 'http://ias.cs.tum.edu/kb/knowrob_mud.owl#', [keep(true)]).
 
-
-
-
-
-
-
-
-
+:- owl_parser:owl_parse('package://knowrob_mud/owl/mud_simulation_map.owl').
+:- rdf_db:rdf_register_ns(mud_map, 'http://ias.cs.tum.edu/kb/mud_simulation_map.owl#', [keep(true)]).
+:- owl_parser:owl_parse('package://knowrob_sim/owl/simulation_map.owl').
+:- rdf_db:rdf_register_ns(sim_map, 'http://ias.cs.tum.edu/kb/simulation_map.owl#', [keep(true)]).
---
+++ knowrob_mud/prolog/rosprolog.pl
@@ -0,0 +1,217 @@
+
+/** <module> rosprolog - For when PrologMUD starts KnowRob (instead of the other way around)
+
+
+@license LGPL
+*/
+
+:- set_prolog_flag(file_loading,verbose).
+:- ensure_loaded('../../../../../src_lib/logicmoo_util/logicmoo_util_all.pl').
+
+:- abolish(dbase_rosprolog,1), dynamic(dbase_rosprolog/1),prolog_load_context(file, Here),asserta(dbase_rosprolog(Here)).
+dbase_rosprolog_dir(DIR):- dbase_rosprolog(Here),absolute_file_name(Here,AHere),file_directory_name(AHere,DIR).
+
+user:file_search_path(knowrob,Where):- dbase_rosprolog_dir(DIR),concat_paths(DIR,'../../../*/',OOO),enumerate_files(OOO,Where),exists_directory(Where).
+user:file_search_path(pack, knowrob).
+user:file_search_path(ros, knowrob).
+
+
+% enumerate_files(Spec,Result):- \+ exists_file_or_dir(Spec),!,expand_file_search_path(Spec,Result).
+enumerate_files(Spec,Result):-expand_file_name(Spec,ResultList),member(NResult,ResultList),normalize_path(NResult,Result).
+
+exists_file_or_dir(X):-exists_file(X),!.
+exists_file_or_dir(X):-is_directory(X).
+is_directory(X):-exists_directory(X).
+
+concat_paths(ParentIn,Child,Result):- enumerate_files(ParentIn,Parent),
+   (is_directory(Parent) -> directory_file_path(Parent,Child,Joined) ; atom_concat(Parent,Child,Joined)),
+   enumerate_files(Joined,Result).
+
+concat_paths([Joined],Result):- !,enumerate_files(Joined,Result).
+concat_paths([ParentIn,Child|MORE],Result):- concat_paths(ParentIn,Child,ResultM),concat_paths([ResultM|MORE],Result).
+
+ros_library_directory(WhereF2):-user:file_search_path(knowrob,W),once((atom_concat(W,'/*/prolog/',O),
+  expand_file_name(O,ListE))),member(Where,ListE),normalize_path(Where,WhereF2).
+
+normalize_path(Where,WhereF2):- absolute_file_name(Where,WhereF),prolog_to_os_filename(WhereF,WhereF1),prolog_to_os_filename(WhereF2,WhereF1),!.
+normalize_path(Where,Where):-!.
+
+
+% ros_package(B):-ros_library_directory(WhereF2),concat_paths(WhereF2,(..),O),file_base_name(O,B).
+ros_package(RosPackName):-user:file_search_path(knowrob,W),once((atom_concat(W,'/*/',O),
+  expand_file_name(O,ListE))),member(Where,ListE),normalize_path(Where,WhereF2),exists_directory(WhereF2),file_base_name(WhereF2,RosPackName).
+  
+
+
+rospack_initfile_path(RosPack,InitFile):-  
+  rospack_package_path(RosPack, PackagePath), 
+  concat_paths([PackagePath,'./prolog/init{.pl,.pl.in,}'],Where),expand_file_name(Where,[InitFile|_]),!.
+
+%% rospack_package_path(+Package, -Path) is nondet.
+%
+% Locate ROS packages on the harddisk using 'rospack find'
+% 
+% @param Package  Name of a ROS package
+% @param Path     Global path of Package determined by 'rospack find'
+% 
+rospack_package_path(Package, Path) :- var(Package),!,ros_package(Package),rospack_package_path(Package, Path).
+rospack_package_path(Package, Path) :- is_absolute_file_name(Package),!,Path=Package.
+rospack_package_path(Package, Path) :- expand_file_search_path(knowrob(Package),Path),exists_directory(Path),!.
+rospack_package_path(Package, Path) :- trace,expand_file_search_path(knowrob(Package),Path),exists_directory(Path),!.
+rospack_package_path(Package, Path) :- must(rospack_find_package_path(Package, Path)).
+
+rospack_find_package_path(Package, Path) :- 
+  nonvar(Package),
+  process_create(path('rospack'), ['find', Package], [stdout(pipe(RospackOutput)), process(PID)]),
+  read_line_to_codes(RospackOutput, C),
+  string_to_list(Path, C),
+  process_wait(PID, _).
+
+
+%% init_ros_package(+PackagePath) is nondet.
+%
+% Initialize a KnowRob package by consulting the prolog/init.pl file
+% 
+% @param PackagePath  Path towards the package to be initialized
+% 
+init_ros_package( Package ) :- do_not_consult_ros_package( Package )  ,!.
+init_ros_package( Package ) :- 
+  rospack_package_path(Package, PackagePath),
+  rospack_initfile_path(PackagePath,InitFile),
+  exists_file(InitFile),
+  set_prolog_flag(file_loading,verbose),
+  must(consult(InitFile)), !.
+
+init_ros_package( Package ):- dmsg(no_initfile(init_ros_package(Package))).
+
+%% register_ros_package(+Package) is nondet.
+%% register_ros_package(+Package, ?AbsoluteDirectory) is nondet.
+%
+% Find and initialize a KnowRob package, i.e. locate the package on the
+% harddisk, add the path to the library search path, and consult the init.pl
+% file that (recursively) initializes the package and its dependencies.
+% 
+% @param Package            Name of a ROS package
+% @param AbsoluteDirectory  Global path to the 'prolog' subdirectory in the given package
+% 
+:- dynamic library_directory_addon/1.
+:- assertz((user:library_directory(AddOnDir):-ros_library_directory(AddOnDir))).
+:- assertz((user:library_directory(AbsoluteDirectory):- library_directory_addon(AbsoluteDirectory))).
+
+:- dynamic(ros_package_initialized/1).
+
+do_not_consult_ros_package( rosprolog ) :-!.
+% do_not_consult_ros_package( jpl ) :-!.
+% do_not_consult_ros_package( thea ) :-!.
+
+register_ros_package( Package, _ ) :- do_not_consult_ros_package( Package )  ,!.
+register_ros_package(Package, _) :-
+  current_predicate(ros_package_initialized/1),
+  ros_package_initialized(Package), !.
+register_ros_package(Package, AbsoluteDirectory) :- 
+  must(register_ros_package_now(Package, AbsoluteDirectory)).
+
+register_ros_package_now(Package, AbsoluteDirectory) :-
+  must(rospack_package_path(Package, PackagePath)),
+  nonvar(PackagePath),
+  atom_concat(PackagePath, '/prolog/', AbsoluteDirectory),
+  retractall(library_directory_addon(AbsoluteDirectory)),
+  asserta(library_directory_addon(AbsoluteDirectory)),
+  assert(user:file_search_path(ros, AbsoluteDirectory)),
+  catch((
+  assert( ros_package_initialized(Package) ),
+  add_ros_package_to_classpath(Package),
+  init_ros_package( Package )),
+      Ex,
+      (retract( ros_package_initialized(Package) ), 
+       throw(Ex))),!.
+
+register_ros_package(Package) :-
+  register_ros_package(Package, _).
+
+
+use_ros_module(Package, FilePath) :-
+  register_ros_package(Package, AbsoluteDirectory),
+  atom_concat(AbsoluteDirectory, FilePath, AbsoluteFilePath),
+  use_module( AbsoluteFilePath ).
+
+
+
+%% add_ros_package_to_classpath(+Package) is nondet.
+% 
+% Adds Java dependencies of Package to the CLASSPATH environment variable
+%
+% @param Package Name of a ROS package
+% 
+add_ros_package_to_classpath(Package):-
+	rospack_package_classpath(Package, PackagePath),	
+	concat_env("CLASSPATH",PackagePath).
+
+%% rospack_package_classpath(+Package, -Path) is nondet.
+% 
+% Calculates the Java dependencies of Package and returns a string to be appended to the CLASSPATH
+%
+% @param Package  Name of a ROS package
+% @param Path     String with the dependencies to be added to the CLASSPATH
+% 
+% calculates java dependencies for classpath
+rospack_package_classpath(Package, Path) :- rospack_package_path(Package, PackPath),atom_concat(PackPath,'**/*.jar',O),!,
+  expand_file_name(O,ListE),atomic_list_concat(ListE,':',Path).
+rospack_package_classpath(Package, Path) :- trace,rospack_package_path(Package, PackPath),atom_concat(PackPath,'**/*.jar',O),!,
+  expand_file_name(O,ListE),atomic_list_concat(ListE,':',Path).
+rospack_package_classpath(Package, Path) :-
+  nonvar(Package),
+  process_create(path('rosrun'), ['rosprolog', 'get_pkg_classpath', Package], [stdout(pipe(RospackOutput)), process(PID)]),
+  read_line_to_codes(RospackOutput, C),
+  string_to_list(Path, C),
+  process_wait(PID, _).
+
+
+prepend_atom_concat(Prefix,Base,Whole):-atom_concat(Prefix,_,Base),!,Whole=Base.
+prepend_atom_concat(OldVal,Suffix,NewVal):-atom_concat(_,Suffix,OldVal),!,NewVal=OldVal.
+prepend_atom_concat(OldVal,Suffix,NewVal):-atom_concat(OldVal,Suffix,NewVal),!.
+
+% concat a value to an environment varible
+% please note: delimiters have to be set within Val, e.g.:
+% '/path/to/lib:/path/to/lib2'
+concat_env(Var,Val):-
+	(getenv(Var,OldVal)
+	->  ((prepend_atom_concat(':',Val,PackagePath),prepend_atom_concat(OldVal,PackagePath,NewVal))) ;
+        (NewVal = Val)),
+	setenv(Var,NewVal).
+
+forall_show(Call):- forall(Call,dmsg(Call)) *-> true; fmt(call_failed(Call)).
+       
+:- register_ros_package(knowrob_common).
+:- register_ros_package(knowrob_roslog_launch).
+:- register_ros_package(knowrob_cram).
+:- register_ros_package(knowrob_sim).
+:- register_ros_package(knowrob_mongo).
+:- register_ros_package(knowrob_vis).
+:- register_ros_package(iai_semantic_maps).
+:- register_ros_package(knowrob_srdl).
+:- forall_show(user:file_search_path(knowrob,_Where)).
+
+% :- forall(no_repeats(ros_package(B)),must(init_ros_package(B))).
+
+:- forall_show(ros_package_initialized(_Where)).
+
+
+end_of_file.
+
+:- init_ros_package(knowrob_common).
+% :- init_ros_package(prolog_perception).
+% :- init_ros_package(knowrob_actions).
+% :- init_ros_package(ias_knowledge_base).
+% :- init_ros_package(comp_missingobj).
+% :- init_ros_package(comp_semantic_map).
+% :- init_ros_package(semweb).
+
+
+
+
+
+
+
+
+
